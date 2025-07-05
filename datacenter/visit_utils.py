8e510ed24f0f0af5b45b05ce5d07ffbdc953140e
maxchuk89 <maxchuk89@gmail.com>
2025-07-05 21:38:58 +0300

    Числа + код

--- datacenter/visit_utils.py
+++ datacenter/visit_utils.py
@@ -1,15 +1,21 @@
 from django.utils.timezone import localtime
 from datetime import timedelta
 
+SECONDS_IN_HOUR = 3600
+SECONDS_IN_MINUTE = 60
+
+
 def get_duration(visit):
     end_time = visit.leaved_at or localtime()
     return end_time - visit.entered_at
 
+
 def format_duration(duration: timedelta) -> str:
     total_seconds = int(duration.total_seconds())
-    hours = total_seconds // 3600
-    minutes = (total_seconds % 3600) // 60
+    hours = total_seconds // SECONDS_IN_HOUR
+    minutes = (total_seconds % SECONDS_IN_HOUR) // SECONDS_IN_MINUTE
     return f"{hours:02d}:{minutes:02d}"
 
+
 def is_visit_long(visit, minutes=60):
-    return get_duration(visit).total_seconds() > minutes * 60
+    return get_duration(visit).total_seconds() > minutes * SECONDS_IN_MINUTE
